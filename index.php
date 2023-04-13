b94a6dcf470b3c0d171ddae944434168fb660e84
momoaab <130633052+momoaab@users.noreply.github.com>
2023-04-13 01:15:59 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+include 'functions.php';
+$pdo = pdo_connect_mysql();
+
+$page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'login';
+
+include $page . '.php';
+?>
